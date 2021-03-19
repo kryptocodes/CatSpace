cda4fef2bc4588bc2e7fe4d8e8f4bf4cbd4ac350
kryptocodes <srivatsantb@gmail.com>
2021-03-19 12:06:22 +0530

    initial commit
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/post'
+import microConfig from "./mikro-orm.config"
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig)
+    await orm.getMigrator().up()
+
+    const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        })
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log("Server running")
+    })
+    // const posts = orm.em.create(Post, {title: "my first post"})
+    // console.log(posts)
+}
+
+main().catch((err) => {
+    console.error(err)
+})
