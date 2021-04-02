ae4d08a8c2c76832987899373f285722ab714996
kryptocodes <srivatsantb@gmail.com>
2021-04-02 13:31:41 +0530

    created query and resolvers
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ backend/src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/post";
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+export default {
+    migrations: {
+        path: path.join(__dirname,'./migrations'), 
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    user: 'postgres',
+    password: 'postgres',
+    dbName: "cats",
+    type: "postgresql",
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
---
+++ backend/src/resolvers/post.ts
@@ -0,0 +1,13 @@
+import { Post } from "../entities/post";
+import { Ctx, Query, Resolver } from "type-graphql";
+import { MyContext } from "src/types";
+
+@Resolver()
+export class PostResolver {
+    @Query(() => [Post])
+    post(
+        @Ctx() {em}: MyContext
+    ): Promise<Post[]>{
+        return em.find(Post, {})
+    }
+}
